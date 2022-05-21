930f53dfba81aeabae66e02d6ffcdca69c6380d4
Son Tran <tranbacson@zen8labs.com>
2022-05-21 17:15:32 +0700

    Implement lazy loading for react-router.

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -3,20 +3,25 @@ import { useEffect, useState } from "react";
 import { RecoilRoot, useRecoilState } from "recoil";
 import { useLocale } from "ttag";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { localeSt } from "src/states";
 import PrivateRoute from "services/components/route/private_route.jsx";
 import NotMatch from "services/components/route/not_match";
 import ScrollToTop from "services/components/scroll_to_top";
 import Utils from "services/helpers/utils";
 import LocaleUtils from "services/helpers/locale_utils";
 import Spinner from "services/components/spinner";
-import Login from "components/auth/login";
-import Profile from "components/auth/profile";
-import Staff from "components/staff";
-import Role from "components/role";
-import Variable from "components/variable";
-import { localeSt } from "src/states";
+
+const Login = React.lazy(() => import("components/auth/login"));
+const Profile = React.lazy(() => import("components/auth/profile"));
+const Staff = React.lazy(() => import("components/staff"));
+const Role = React.lazy(() => import("components/role"));
+const Variable = React.lazy(() => import("components/variable"));
 
 Utils.responseIntercept();
+
+function FallBack() {
+    return <div>Loading...</div>;
+}
 
 function Index() {
     const [dataLoaded, setDataLoaded] = useState(false);
@@ -37,14 +42,56 @@ function Index() {
             <BrowserRouter>
                 <ScrollToTop />
                 <Routes>
-                    <Route path="/login" element={<Login />} />
+                    <Route
+                        path="/login"
+                        element={
+                            <React.Suspense fallback={<FallBack />}>
+                                <Login />
+                            </React.Suspense>
+                        }
+                    />
                     <Route path="/" element={<PrivateRoute />}>
-                        <Route path="/" element={<Profile />} />
-                        <Route path="/staff" element={<Staff />} />
-                        <Route path="/role" element={<Role />} />
-                        <Route path="/variable" element={<Variable />} />
+                        <Route
+                            path="/"
+                            element={
+                                <React.Suspense fallback={<FallBack />}>
+                                    <Profile />
+                                </React.Suspense>
+                            }
+                        />
+                        <Route
+                            path="/staff"
+                            element={
+                                <React.Suspense fallback={<FallBack />}>
+                                    <Staff />
+                                </React.Suspense>
+                            }
+                        />
+                        <Route
+                            path="/role"
+                            element={
+                                <React.Suspense fallback={<FallBack />}>
+                                    <Role />
+                                </React.Suspense>
+                            }
+                        />
+                        <Route
+                            path="/variable"
+                            element={
+                                <React.Suspense fallback={<FallBack />}>
+                                    <Variable />
+                                </React.Suspense>
+                            }
+                        />
                     </Route>
-                    <Route path="*" element={<NotMatch />} />
+                    <Route
+                        path="*"
+                        element={
+                            <React.Suspense fallback={<FallBack />}>
+                                <NotMatch />
+                            </React.Suspense>
+                        }
+                    />
                 </Routes>
             </BrowserRouter>
         </div>
