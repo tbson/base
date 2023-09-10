6419ed1b215febd5c95704a9cc8d031bde2d2351
Son Tran <tbson87@gmail.com>
2023-09-11 06:45:39 +0700

    Add missing import.

--- api/module/custom_cmd/management/commands/cmd_user_seeding.py
+++ api/module/custom_cmd/management/commands/cmd_user_seeding.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from apps.customer.models import Customer
 
 User = get_user_model()
